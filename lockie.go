fe595226fe2e05dd1f1ee7def6795544201db7e1
itsMontoya <josh@montoya.io>
2016-03-02 10:59:48 -0800

    Adding some more thorough commenting to lockie.go

--- lockie.go
+++ lockie.go
@@ -12,18 +12,23 @@ func NewLockie() *Lockie {
 
 // Lockie is the primary interface for locking/unlocking
 type Lockie struct {
+	// Lock state
+	// 0 represents an unlocked state
+	// 1 represents a locked state
 	lock int64
 }
 
 // Lock acquires a write-lock
 func (l *Lockie) Lock() {
-	// Acquire a write-lock
+	// Loop until we are able to swap value of l.lock from 0 to 1
 	for !atomic.CompareAndSwapInt64(&l.lock, 0, 1) {
+		// Allow other go routines to utilize some CPU time
 		runtime.Gosched()
 	}
 }
 
-// Unlock releases a write-lock
+// Unlock releases a lock
 func (l *Lockie) Unlock() {
+	// Swaps the value of l.lock to 0
 	atomic.StoreInt64(&l.lock, 0)
 }
